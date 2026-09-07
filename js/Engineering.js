0b8010703cd4aa1edd72cf6a4090f58cc558a1bd
shidothemediocre <mainthukhant123@gmail.com>
2026-09-08 01:58:41 +0630

    Implement click animation for university cards
    Added click animation to university cards.

--- js/Engineering.js
+++ js/Engineering.js
@@ -48,3 +48,18 @@ window.addEventListener("load", () => {
     });
 
 });
+
+// Click animation
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        card.style.transform = "scale(0.95)";
+
+        setTimeout(() => {
+            card.style.transform = "";
+        }, 150);
+
+    });
+
+});
